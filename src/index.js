4ee47190b8bf1657513c8acd7bb4cc5bd4f0ed9c
Luna <boreum4151@gmail.com>
2019-04-18 14:34:36 +0900

    add styles with styled-components

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {ApolloProvider} from 'react-apollo';
 import client from './apollo';
+import GlobalStyle from "./globalStyles"; 
 
 ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
