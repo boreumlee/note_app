3746ec96f2e6ab353221f928f0bd9fc69b338ce5
Luna <boreum4151@gmail.com>
2019-04-20 19:48:14 +0900

    change edit to note in notes.js

--- src/Routes/Notes/Notes.js
+++ src/Routes/Notes/Notes.js
@@ -75,7 +75,7 @@ export default class NotesContainer extends React.Component{
                 <Query query={GET_NOTES}>
                     {({data})=> 
                         data.notes ? data.notes.map(note => (
-                            <Link to={`/edit/${note.id}`} key={note.id}>
+                            <Link to={`/note/${note.id}`} key={note.id}>
                                 <Note>{note.title}</Note>
                             </Link>
                         ))
